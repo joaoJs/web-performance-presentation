bcb4fbcb050bd6d5abdcebcb567b2d4f49e62865
Joao Campos <j.campos4g@gmail.com>
2020-06-02 05:46:09 -0400

    adds brotli compress

--- build-utils/presets/webpack.compress.js
+++ build-utils/presets/webpack.compress.js
@@ -1,5 +1,20 @@
-const CompressionPlugin = require('compression-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin')
+const BrotliPlugin = require('brotli-webpack-plugin')
 
-module.exports = () => ({
-  plugins: [new CompressionPlugin()],
-})
+module.exports = {
+  plugins: [
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.7
+    }),
+    new BrotliPlugin({
+      asset: '[path].br[query]',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.7
+    })
+  ]
+}
